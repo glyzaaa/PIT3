2dcc05cb4b8dc199ff7df09407d9002c364c15a8
glyzaaa <apusglyzamae@gmail.com>
2025-04-08 10:10:24 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +84,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES["default"] = dj_database_url.parse("postgresql://todo_project_dlby_user:XTzMSN66MRIvOXJ8g7S1FenLX7i9O1IL@dpg-cvoig0juibrs73brqh70-a/todo_project_dlby")
 
 
 # Password validation
